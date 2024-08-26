db085d9567b936371f6f70ec414c05b90131ef9b
fandi-ahmad <fandi4160@gmail.com>
2024-08-26 12:51:55 +0800

    make default empty note

--- src/function/calendar/generateDateByMonth.ts
+++ src/function/calendar/generateDateByMonth.ts
@@ -1,13 +1,3 @@
-// import { MonthType } from "../../interface";
-
-// const generateDateByMonth = (data: MonthType) => {
-//   // Buat array dari data bulan, hilangkan 'd' dan '0' jika ada
-//   return Object.entries(data).map(([key, value]) => {
-//     const date = key.replace('d', '').replace(/^0/, ''); // Hilangkan 'd' dan '0' depan
-//     return { date, image: value.image };
-//   });
-// }
-
 const generateDateByMonth = (year: number, month: number) => {
   // Fungsi yang menghasilkan tanggal dalam bulan yang dipilih
   let dates = [];
@@ -17,7 +7,7 @@ const generateDateByMonth = (year: number, month: number) => {
     dates.push({
       date: `d${i.toString().padStart(2, '0')}`, // format d01, d02, ...
       image: '',
-      note: `This is day ${i.toString().padStart(2, '0')}`,
+      note: '',
       month: `m${month}`,
       year: `y${year}`
     });
