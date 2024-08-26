f00c5aafce73f3e26472d03c74df1be2eae6938f
fandi-ahmad <fandi4160@gmail.com>
2024-08-26 12:53:19 +0800

    modified image size in date

--- src/components/SingleDate.tsx
+++ src/components/SingleDate.tsx
@@ -7,8 +7,8 @@ interface SingleDateProps {
 
 const SingleDate = (props: SingleDateProps) => {
   return (
-    <div onClick={props.onClick} className="rounded-full w-8 h-8 flex justify-center items-center font-medium mx-auto overflow-hidden cursor-pointer hover:bg-slate-300 duration-200">
-      {props.image && <img src={props.image} alt="" className='w-full relative z-10' />}
+    <div onClick={props.onClick} className="rounded-full w-7 h-7 flex justify-center items-center font-medium mx-auto overflow-hidden cursor-pointer hover:bg-slate-300 duration-200">
+      {props.image && <img src={props.image} alt="" className='relative z-10 w-7 h-7 object-fill' />}
       <p className={`absolute z-20  ${props.image && 'text-white'}`}>
         {props.date.replace('d', '').replace(/^0/, '')}
       </p>
