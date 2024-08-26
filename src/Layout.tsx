4e34c9454de2db8fb8022bcf5eb3e25885ff9ed3
fandi-ahmad <fandi4160@gmail.com>
2024-08-26 12:52:20 +0800

    modified layout

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({children}) => {
   return (
     <div className="bg-slate-400 font-body text-gray-800">
-      <div className='bg-white relative max-w-xl mx-auto h-screen aspect-[9/16]'>
+      <div className='bg-white relative max-w-xl mx-auto h-screen'>
         <div className="relative min-h-screen">
           <div className="absolute inset-0 bg-[url('/bg1.jpg')] bg-cover bg-center blur-sm"></div>
           <div className="relative z-10 h-screen flex justify-center items-center">
