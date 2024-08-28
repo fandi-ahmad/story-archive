261325eefe05b4abc85c0b2f10524435978cd75b
fandi-ahmad <fandi4160@gmail.com>
2024-08-28 23:37:12 +0800

    modified image in date

--- src/components/SingleDate.tsx
+++ src/components/SingleDate.tsx
@@ -8,7 +8,7 @@ interface SingleDateProps {
 const SingleDate = (props: SingleDateProps) => {
   return (
     <div onClick={props.onClick} className="rounded-full w-7 h-7 flex justify-center items-center font-medium mx-auto overflow-hidden cursor-pointer hover:bg-slate-300 duration-200">
-      {props.image && <img src={props.image} alt="" className='relative z-10 w-7 h-7 object-fill' />}
+      {props.image && <img src={props.image} alt="" className='relative z-10 w-7 h-7 object-cover' />}
       <p className={`absolute z-20  ${props.image && 'text-white'}`}>
         {props.date.replace('d', '').replace(/^0/, '')}
       </p>
